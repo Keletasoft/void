f175b63c9910c558441659d79446d9849a7dbbc4
auzou <senecal.c.pro@hotmail.fr>
2023-04-15 15:42:50 +0200

    camera widget

---
+++ src/component/camera.h
@@ -0,0 +1,46 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <QWidget>
+#include <QPushButton>
+
+/**
+ * @namespace lgs::component
+ */
+namespace lgs::componnent
+{
+
+/**
+ * @brief The Camera class
+ */
+class Camera
+{
+    Q_OBJECT
+
+    private :
+        QPushButton *take;
+
+
+    public :
+
+        /**
+         * @brief Camera
+         * @param parent
+         */
+        Camera(QWidget *parent = nullptr);
+
+        /**
+         * @brief ~Camera
+         */
+        virtual ~Camera() noexcept;
+
+    public :
+
+        /**
+         * @brief move
+         */
+        void move() const;
+};
+
+} // END NAMESPACE lgs::camera
+
+#endif // CAMERA_H
