ccf5a64cc79b6289b85d295e8bd5af06d573173b
auzou <senecal.c.pro@hotmail.fr>
2023-04-18 21:10:39 +0200

    Update translation.cpp

--- src/translation.cpp
+++ src/translation.cpp
@@ -1,2 +1,13 @@
 #include "translation.h"
 
+lgs::Translation::Translation(QObject *parent)
+    : QObject(parent)
+{ }
+
+lgs::Translation::~Translation() noexcept
+{ }
+
+void lgs::Translation::load()
+{
+
+}
