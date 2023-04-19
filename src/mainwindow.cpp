7c673ff214ac59d729b43729fb5df1aa0918555b
auzou <senecal.c.pro@hotmail.fr>
2023-04-19 22:14:04 +0200

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,6 +3,11 @@
 lgs::MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->authentication = new lgs::component::Authentication(this);
+
+    if(!this->authentication->isAuthenticated())
+        this->setCentralWidget(this->authentication);
+
 
 }
 
