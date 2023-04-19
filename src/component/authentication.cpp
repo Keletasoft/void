bfac8cb3e2713c318d5fb25ad300526c2bacacd0
auzou <senecal.c.pro@hotmail.fr>
2023-04-19 22:13:58 +0200

    Update authentication.cpp

--- src/component/authentication.cpp
+++ src/component/authentication.cpp
@@ -5,7 +5,19 @@
 lgs::component::Register::Register(QWidget *parent)
     : QWidget(parent)
 {
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
+    this->companyCode = new QLineEdit(this);
+    mainLayout->addWidget(this->companyCode);
+
+    this->name = new QLineEdit(this);
+    mainLayout->addWidget(this->name);
+
+    this->email = new QLineEdit(this);
+    mainLayout->addWidget(this->email);
+
+    this->password = new QLineEdit(this);
+    mainLayout->addWidget(this->password);
 }
 lgs::component::Register::~Register() noexcept
 { }
@@ -20,7 +32,19 @@ void lgs::component::Register::checkRegister()
 lgs::component::Login::Login(QWidget *parent)
     : QWidget(parent)
 {
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
+    this->companyCode = new QLineEdit(this);
+    mainLayout->addWidget(this->companyCode);
+
+    this->name = new QLineEdit(this);
+    mainLayout->addWidget(this->name);
+
+    this->email = new QLineEdit(this);
+    mainLayout->addWidget(this->email);
+
+    this->password = new QLineEdit(this);
+    mainLayout->addWidget(this->password);
 }
 
 lgs::component::Login::~Login() noexcept
@@ -41,6 +65,11 @@ lgs::component::Authentication::Authentication(QWidget *parent)
 
 lgs::component::Authentication::~Authentication() noexcept
 { }
+
+bool lgs::component::Authentication::isAuthenticated() const
+{
+    return false;
+}
 
 void lgs::component::Authentication::registerSucces()
 {
